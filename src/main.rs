9ec47b99f4a8d9f6b4cf1c243e7135aac2c718d6
Mike Allen <mpallenjnr@gmail.com>
2023-02-28 09:01:16 -0600

    unwrap method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 
 #![allow(unused)]
 fn main() {
-let result = std::fs::read_to_string("test.txt");
-let content = match result {
-    Ok(content) => { content },
-    Err(error) => { panic!("Can't deal with {}, just exit here", error); }
-};
+let content = std::fs::read_to_string("text.txt").unwrap();
+
 println!("file content: {}", content);
 }
