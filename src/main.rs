0f8d711d94aae2b8d311c869696832cff9a78f68
Mike Allen <mpallenjnr@gmail.com>
2023-02-28 09:10:50 -0600

    return error with question mark method, used in box error

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let result = std::fs::read_to_string("test.txt");
-    let content = match result {
-        Ok(content) => { content },
-        Err(error) => { return Err(error.into()); }
-    };
+    let content = std::fs::read_to_string("test.txt")?;
+
+    println!("file content: {}", content);
     Ok(())
-    }
+}
