40a582969d7402416a049ba9361e51a23fcf7c08
Mike Allen <mpallenjnr@gmail.com>
2023-02-28 08:59:48 -0600

    print results

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+
+#![allow(unused)]
 fn main() {
-    println!("Hello, world!");
+let result = std::fs::read_to_string("text.txt");
+match result {
+    Ok(content) => { println!("File content: {}", content); }
+    Err(error) => { println!("Oh noes: {}", error); }
+}
 }
